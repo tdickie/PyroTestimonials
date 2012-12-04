f9057f3af80139ea6139b72bad7abf2bd82d7378
Tom Dickie <tom.dickie@gmail.com>
2012-12-04 00:01:20 +0000

    Created testimonials module using the steams driver

---
+++ controllers/testimonials.php
@@ -0,0 +1,45 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Testimonials extends Public_Controller
+{
+
+    /**
+     * The constructor
+     * @access public
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->lang->load('testimonials');
+        $this->load->driver('Streams');
+    }
+     /**
+     * List all TESTIMONIALSs
+     *
+     * We are using the Streams API to grab
+     * data from the testimonialss database. It handles
+     * pagination as well.
+     *
+     * @access	public
+     * @return	void
+     */
+    public function index()
+    {
+        $params = array(
+            'stream' => 'testimonials',
+            'namespace' => 'testimonials',
+            'paginate' => 'yes',
+            'pag_segment' => 4
+        );
+
+        $this->data->testimonials = $this->streams->entries->get_entries($params);
+
+        // Build the page
+        $this->template->title($this->module_details['name'])
+                ->build('index', $this->data);
+    }
+
+}
+
+/* End of file testimonials.php */
